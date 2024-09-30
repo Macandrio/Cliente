db9df9fab21a582be7923ac85e5e31877ed81689
Macandrio <alejandro.gomez.rivera.al@iespoligonosur.org>
2024-09-30 09:20:34 +0200

    Delete JavaScript directory

--- JavaScript/Eje_2.js
+++
@@ -1,6 +0,0 @@
-//Diseñar un algoritmo para hallar el valor absoluto de un número leído de teclado y
-//presentarlo en pantalla
-
-var num = window.prompt('Ingresa un número');
-
-alert('El valor absoluto es = ' + Math.abs(num));
