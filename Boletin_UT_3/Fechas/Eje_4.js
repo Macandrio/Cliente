30b98123084a668c0748a55f0726086ec340b4de
Alejandro Gomez <alejandro.gomez.rivera.al@iespoligonosur.org>
2024-10-07 14:42:03 +0200

    subida

---
+++ Boletin_UT_3/Fechas/Eje_4.js
@@ -0,0 +1,12 @@
+/*Calcular de un conjunto de fechas la menor, la mayor y la diferencia en segundos
+entre ambas*/
+
+
+
+do{
+
+    var fec = (prompt('introduce una fecha'));
+    var lis_fecha = [];
+    lis_fecha.push[fec];
+    
+}while(num == 1);
