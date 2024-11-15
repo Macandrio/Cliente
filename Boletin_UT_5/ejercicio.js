608080b930e073e693f0e9e435c9d0fe3381a4f9
Alejandro Gomez <alejandro.gomez.rivera.al@iespoligonosur.org>
2024-11-15 12:41:14 +0100

    subir

---
+++ Boletin_UT_5/ejercicio.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', inicializar, flase);
+
+function inicializar(){
+    
+}
+
+function recogerDatos() {
+    // Seleccionar los elementos por su id
+    var nombre = document.getElementById('nombre').value;
+    var apellidos = document.getElementById('apellidos').value;
+    var prefijo = document.getElementById('prefijo').value;
+    var movil = document.getElementById('movil').value;
+}
